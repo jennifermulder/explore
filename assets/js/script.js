1260b1109d3e270083f507e35119c231250a66a5
nmcanall <nmcanall@citadel.edu>
2020-07-02 20:07:37 -0700

    pull nearby trail information

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,9 @@
 // Calls to GeoCoding API by Google and returns lat/long
 var searchAddress = function(address) {
 
-    var apiURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyAWVEsp8JBkgZyxhUjCaO2o7XN61ULxz6w";
+    var apiURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + 
+                    address + 
+                    "&key=AIzaSyAWVEsp8JBkgZyxhUjCaO2o7XN61ULxz6w";
 
     // Use first fetch to get latitude and longitude of user-input city
     fetch(apiURL)
@@ -13,29 +15,28 @@ var searchAddress = function(address) {
     .then(function(googleData) {
         var lat = googleData.results[0].geometry.location.lat;
         var lon = googleData.results[0].geometry.location.lng;
-
         console.log(lat, lon);
-        // // Once we get lat/long, use second fetch to retrieve data from OneCall API
-        // var hikingURL = "https://www.hikingproject.com/data/get-trails?" + 
-        //         "lat=" + lat + 
-        //         "&lon=" + lon +
-        //         "&key=200820241-a86aed042606dfdfd7bf8931045a1ecf"
-        // return fetch(hikingURL)
+        // Once we get lat/long, use second fetch to retrieve data from HikingProject API
+        var hikingURL = "https://www.hikingproject.com/data/get-trails?" + 
+                "lat=" + lat + 
+                "&lon=" + lon +
+                "&key=200820241-a86aed042606dfdfd7bf8931045a1ecf"
+        return fetch(hikingURL)
     })
 
-    // // Response to inner fetch
-    // .then(function(response) {
-    //     return response.json();
-    // })
-    // // Function that provides functionality to build page
-    // .then(function(data) {
-    //     console.log(data);
-    // })
+    // Response to inner fetch
+    .then(function(response) {
+        return response.json();
+    })
+    // Function that provides functionality to build page
+    .then(function(data) {
+        console.log(data);
+    })
 
     // Error if bad connection to server
-    // .catch(function(error) {
-    //     alert("Unable to find the requested city.");
-    // });
+    .catch(function(error) {
+        alert("Unable to find the requested city.");
+    });
 };
 
 searchAddress("37388");
